8e9cec431b262d56763d0a7e425734b935f96c9f
Ramzan <rumkkhan@gmail.com>
2020-01-13 17:08:13 +0530

    remove

--- src/components/API/GetApi.js
+++ src/components/API/GetApi.js
@@ -4,13 +4,13 @@ import Styles from './GetApi.css';
 import S3FileUpload from 'react-s3';
 import AWS from  'aws-sdk'
 
-const config = {
-    bucketName: 'saralclouldexcelupload',
-    region: 'ap-south-1',
-    accessKeyId: 'AKIAYPWLFEAY7QTJCOW3',
-    secretAccessKey: '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',
-    metaTags: 'idsdfasdfasdfa'
-}
+// const config = {
+//     bucketName: 'saralclouldexcelupload',
+//     region: 'ap-south-1',
+//     accessKeyId: 'AKIAYPWLFEAY7QTJCOW3',
+//     secretAccessKey: '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',
+//     metaTags: 'idsdfasdfasdfa'
+// }
 export default function GetAPI() {
 
    const [result, setResult] = useState([]);
@@ -30,55 +30,55 @@ export default function GetAPI() {
     setResult(response.data.hits)
    }
 
-  const fileSelectedHandler = event => {
-debugger
-AWS.config.region = 'ap-south-1'; // Region
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'ap-south-1:2f16e677-ee43-476e-a3c7-376b96c51260',
-});
-AWS.config.credentials.get(function(err) {
-  if (err) alert(err);
-  console.log(AWS.config.credentials);
-});
-var bucketName = 'your-bucket'; // Enter your bucket name
-var buckett = new AWS.S3({
-    params: {
-        Bucket: 'exceluploadd',
-Metadata: {
-      'x-amz-meta-my-key': 'some-value',
-      'x-amz-meta-my-clientId': 'relyonsoftteect',
-    },
-    },
-});
-setBucket(
-  buckett
+//   const fileSelectedHandler = event => {
+// debugger
+// AWS.config.region = 'ap-south-1'; // Region
+// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+//     IdentityPoolId: 'ap-south-1:2f16e677-ee43-476e-a3c7-376b96c51260',
+// });
+// AWS.config.credentials.get(function(err) {
+//   if (err) alert(err);
+//   console.log(AWS.config.credentials);
+// });
+// var bucketName = 'your-bucket'; // Enter your bucket name
+// var buckett = new AWS.S3({
+//     params: {
+//         Bucket: 'exceluploadd',
+// Metadata: {
+//       'x-amz-meta-my-key': 'some-value',
+//       'x-amz-meta-my-clientId': 'relyonsoftteect',
+//     },
+//     },
+// });
+// setBucket(
+//   buckett
   
-)
-    var fil = event.target.files[0];
+// )
+//     var fil = event.target.files[0];
    
    
-     setFile(fil);
-     console.log(filess)
-}
-  const fileUploadHandler = event => {
-    console.log(filess,"++++++++")
+//      setFile(fil);
+//      console.log(filess)
+// }
+//   const fileUploadHandler = event => {
+//     console.log(filess,"++++++++")
    
-    var objKey = filess.name;
-    var params = {
-        Key: objKey,
-        ContentType: filess.type,
-        Body: filess,
-        ACL: 'public-read'
-    };
+//     var objKey = filess.name;
+//     var params = {
+//         Key: objKey,
+//         ContentType: filess.type,
+//         Body: filess,
+//         ACL: 'public-read'
+//     };
 
-    bucket.putObject(params, function(err, data) {
-        if (err) {
+//     bucket.putObject(params, function(err, data) {
+//         if (err) {
            
-        } else {
+//         } else {
            
-        }
-    });
-  }
+//         }
+//     });
+//   }
    return (
        
        <div>
